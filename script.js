9ddf28417ee95e7f4683c49157a20f935c6d40c7
shivamshinde26 <103212288+shivamshinde26@users.noreply.github.com>
2024-06-01 15:13:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "6d26f390d6msh2c604512c012e0bp1a71b9jsn12de46ddb8d6",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
+};
+const getWeather=(city)=>{
+	cityName.innerHTML=city
+	fetch("https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city="+city,options)
+	.then((response) => response.json())
+	.then((response) => {
+	  console.log(response);
+	//   cloud_pct.innerHTML=response.cloud_pct
+	  temp.innerHTML=response.temp
+	//   temp2.innerHTML=response.temp
+	  feels_like.innerHTML=response.feels_like
+	  humidity.innerHTML=response.humidity
+	//   humidity2.innerHTML=response.humidity
+	  min_temp.innerHTML=response.min_temp
+	  max_temp.innerHTML=response.max_temp
+	  wind_speed.innerHTML=response.wind_speed
+	//   wind_speed2.innerHTML=response.wind_speed
+	  wind_degrees.innerHTML=response.wind_degrees
+	  sunrise.innerHTML=response.sunrise
+	  sunset.innerHTML=response.sunset
+	})
+	.catch((err) => console.error(err));
+}
+submit.addEventListener("click",(e)=>{
+	e.preventDefault()
+	getWeather(city.value)
+})
+getWeather("Delhi")
+we.addEventListener("click",(e)=>{
+	e.preventDefault()
+	getWeather("Mumbai")
+})
+we2.addEventListener("click",(e)=>{
+	e.preventDefault()
+	getWeather("Delhi")
+})
+we3.addEventListener("click",(e)=>{
+	e.preventDefault()
+	getWeather("Kolkata")
+})
+darkmode.onclick=function(){
+	document.body.classlist.toggle("dark-theme")
+}
+function dark(){
+	document.getElementById('icon')
+	
+}
